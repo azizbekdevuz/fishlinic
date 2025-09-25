cbc8a3b1a6682051ae9af32e6a0cb6191e1a69bb
Azizbek Arzikulov <azizbek.dev.ac@gmail.com>
2025-09-26 06:47:36 +0900

    fix(next.config): remove outputFileTracingRoot to avoid vercel errors while deployment

--- next.config.ts
+++ next.config.ts
@@ -4,8 +4,6 @@ import path from "path";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
-  // Fix workspace root inference when multiple lockfiles exist (monorepo-like setup)
-  outputFileTracingRoot: path.resolve(__dirname, ".."),
 };
 
 export default nextConfig;
